faf606e61200c4f71b6411112c2f09d0e870843e
chenq <chenq@ersoft.cn>
2018-06-12 10:35:31 +0800

    Initial commit

---
+++ spring-config-learn/src/main/java/com/github/yulechen/springconfiglearn/SpringConfigLearnApplication.java
@@ -0,0 +1,12 @@
+package com.github.yulechen.springconfiglearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigLearnApplication.class, args);
+	}
+}
