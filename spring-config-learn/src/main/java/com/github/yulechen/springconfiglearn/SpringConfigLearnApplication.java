a72d086aa8ee565d466dcc69ab45e407c9a4e976
chenq <chenq@ersoft.cn>
2018-06-12 16:57:42 +0800

    debug -config server

--- spring-config-learn/src/main/java/com/github/yulechen/springconfiglearn/SpringConfigLearnApplication.java
+++ spring-config-learn/src/main/java/com/github/yulechen/springconfiglearn/SpringConfigLearnApplication.java
@@ -2,8 +2,31 @@ package com.github.yulechen.springconfiglearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ *
+ *
+ * # Bootstrap components
+ org.springframework.cloud.bootstrap.BootstrapConfiguration=\
+ org.springframework.cloud.config.server.bootstrap.ConfigServerBootstrapConfiguration,\
+ org.springframework.cloud.config.server.config.EncryptionAutoConfiguration
+
+ # Application listeners
+ org.springframework.context.ApplicationListener=\
+ org.springframework.cloud.config.server.bootstrap.ConfigServerBootstrapApplicationListener
+
+ # Autoconfiguration
+ org.springframework.boot.autoconfigure.EnableAutoConfiguration=\
+ org.springframework.cloud.config.server.config.ConfigServerAutoConfiguration,\
+ org.springframework.cloud.config.server.config.EncryptionAutoConfiguration
+
+ *
+ *
+ */
 @SpringBootApplication
+//EnableConfigServer 生成 maker 类，ConfigServerAutoConfiguration 依据生成的maker 来配置
+@EnableConfigServer
 public class SpringConfigLearnApplication {
 
 	public static void main(String[] args) {
