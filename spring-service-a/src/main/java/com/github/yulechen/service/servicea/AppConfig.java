276a089f8869714559d8894aed0687185c4080f0
chenq <chenq@ersoft.cn>
2018-06-13 18:50:22 +0800

    add config client test

---
+++ spring-service-a/src/main/java/com/github/yulechen/service/servicea/AppConfig.java
@@ -0,0 +1,29 @@
+package com.github.yulechen.service.servicea;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class AppConfig {
+    //@Value("${name}")
+    private String name;
+
+   // @Value("${age}")
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
